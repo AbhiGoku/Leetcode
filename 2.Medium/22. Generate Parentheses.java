d803c26a71466d9f53961964f177db9106483a25
Abhishek L S <abhishekk.l.s1998@gmail.com>
2024-11-17 02:28:23 -0500

    added the kth largest element

--- 2.Medium/22. Generate Parentheses.java
+++ 2.Medium/22. Generate Parentheses.java
@@ -1,18 +1,21 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
   public List<String> generateParenthesis(int n) {
-      List<String> output=new ArrayList<>();
-      backtracking(output,n,"",0,0);
-      return output;
+    List<String> output = new ArrayList<>();
+    backtracking(output, n, "", 0, 0);
+    return output;
   }
 
-  private void backtracking(List<String> output,int max,String result,int open,int close){
-      if(result.length()==max*2){
-          output.add(result);
-          return;
-      }
-      if(open<max)backtracking(output,max,result+"(",open+1,close);
-      if(close<open)backtracking(output,max,result+")",open,close+1);
+  private void backtracking(List<String> output, int max, String result, int open, int close) {
+    if (result.length() == max * 2) {
+      output.add(result);
+      return;
+    }
+    if (open < max)
+      backtracking(output, max, result + "(", open + 1, close);
+    if (close < open)
+      backtracking(output, max, result + ")", open, close + 1);
   }
 }
